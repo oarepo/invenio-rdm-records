40b6fa1de7697e0e5c8d0a7cb37547093426545e
senyaaa <pogodars@cvut.cz>
2025-10-29 09:51:47 +0100

    feat: add copyright and license information to extractor and processor files

--- invenio_rdm_records/services/files/extractors/reply_stream.py
+++ invenio_rdm_records/services/files/extractors/reply_stream.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CESNET, a.l.e.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
 import os
 
 
